e728f13fa43eb709a439f49e94b03e9ba9121652
Guillermo Oliva <guillermooliva360g@gmail.com>
2026-02-28 08:53:23 -0600

    tercer cambio

--- Algoritmo.cs
+++ Algoritmo.cs
@@ -31,6 +31,7 @@ public class Algoritmo
         int n = arr.Length;
         for (int i = 0; i < n - 1; i++)
         {
+
             for (int j = 0; j < n - i - 1; j++)
             {
                 if (arr[j] > arr[j + 1])
